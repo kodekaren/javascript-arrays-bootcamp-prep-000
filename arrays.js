6517ea8e5c0d5c1e21e0eb4cd646cd910c87d59e
karen ercoli <karenercoli@gmail.com>
2018-11-01 18:12:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,7 +17,7 @@ function addElementToEndOfArray(chocolateBars, candyString) {
 }
 
 function destructivelyAddElementToEndOfArray(chocolateBars, candyString) {
-  chocolateBars.unshift(-1)
+  chocolateBars.unshift("foo")
   return chocolateBars
 }
 
