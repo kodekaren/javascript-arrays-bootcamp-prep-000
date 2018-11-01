6c55e1f6f2f72ec52b236e28104d2142aa1948c5
karen ercoli <karenercoli@gmail.com>
2018-11-01 19:17:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,7 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
 chocolateBars.unshift(candyString)
 return chocolateBars
 }
+
+function addElementToEndOfArray(chocolateBars, candyString) {
+  return [... chocolateBars,candyString]
+}
