49f9ba51d1c355be6408079c6d7308ea822e0661
karen ercoli <karenercoli@gmail.com>
2018-11-01 18:44:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,7 +12,6 @@ return chocolateBars
 }
 
 function addElementToEndOfArray(chocolateBars, candyString) {
-  [chocolateBars, ...candyString]
   chocolateBars = [chocolateBars, ...candyString]
   return chocolateBars
 }
