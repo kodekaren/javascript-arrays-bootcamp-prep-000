87ac2d635a20650c30a511ef9699d451736a8441
karen ercoli <karenercoli@gmail.com>
2018-11-01 19:14:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
 var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`]
+var candyString = "foo"
+
+function addElementToBeginningOfArray(chocolateBars, candyString) {
+  return chocolateBars=[candyString, ...chocolateBars]
+}
